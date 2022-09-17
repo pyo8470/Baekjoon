e92c6a2ca601b44bcfe8ba057190c5425681f4f5
pyo8470 <rkswlska789@naver.com>
2022-09-17 15:18:08 +0900

    첫 시도 땐 왜 안되었었을까

--- 2022_08/2108.py
+++ 2022_08/2108.py
@@ -4,22 +4,29 @@ count =[0]*8001 # -4000 : 0, 4000: 8000, 0: 4000
 for i in range (N):
     x.append(int(input()))
 Avg=(sum(x)/N)
-print(round(Avg))       # 평균
+print(round(Avg))
+# 평균
 
 
-x=sorted(x)             # 정렬
-print(x[round(N/2)])    # 중앙값
+
+x.sort()           # 정렬
+print(x[int(N/2)])    # 중앙값
+
+# 최빈
 for i in range(N):
     count[x[i]+4000]=count[x[i]+4000]+1
-tmp = max(count)
-index_first=count.index(tmp)
-count[index_first]=-1
-if max(count)==tmp:
-    index_second=count.index(max(count))
-    print(index_second-4000)
-else :
-    print(index_first-4000)
-
-Range=x[N-1]-x[0]       ## 범위
+max_num=max(count)
+max_Cnt=0
+for i in range(0,8001):
+    if max_Cnt==0 and count[i]==max_num:
+        Final= i
+        
+        max_Cnt=max_Cnt+1
+    elif max_Cnt==1 and count[i]==max_num :
+        Final = i
+        
+        break
+print(Final-4000)
+Range=max(x)-min(x)      ## 범위
 print(Range)    
     
