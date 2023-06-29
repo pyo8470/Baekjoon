a8324008c12e91b6cd406ca9f1f0da87327be337
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2023-06-29 17:11:16 +0900

    [Silver I] Title: IOIOI, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,14 +1,15 @@
 import sys
 N = int(input())
 M = int(input())
-S = list(sys.stdin.readline().rstrip())
+S = sys.stdin.readline()
 Pn = 'I'+('OI'*N)
 count =0
-for i in range(M):
-    if i < M-2*N and S[i] =='I':
-        temp=S[i]
-        for j in range(N):
-            temp+=(S[i+2*j+1]+S[i+2*j+2])
-        if(temp == Pn):
-            count+=1
+i=0
+while i<M-2*N:
+    if S[i] == 'I' and S[i:i+2*N+1] == Pn:
+        count+=1
+        # 이미 패턴 발견 -> 다음 검사
+        i+=2
+    else:
+        i+=1
 print(count)
