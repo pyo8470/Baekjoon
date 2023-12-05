2ff6bc52e9fd45f2a849436dfe175ba3fe38f892
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2023-12-05 21:01:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.06 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,4 @@
+import collections
 def solution(participant, completion):
-    answer = ''
-    part = {}
-    for a in participant:
-        part[a] = part[a] + 1 if a in part else 1
-    for k in completion:
-        part[k] -=1
-    for key,value in part.items():
-        if value != 0 :
-            answer=key
-    return answer
+    return list((collections.Counter(participant)-collections.Counter(completion)).keys())[0]
+    
