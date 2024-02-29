14f398661a2c80f28c6009c2c9925b4e04a95a0d
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-02-29 23:43:32 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,20 +1,18 @@
 def solution(X, Y):
     answer = ''
-    XList = [0 for i in range(10)]
-    YList = [0 for i in range(10)]
-    for x in X:
-        XList[int(x)] +=1
-    for y in Y:
-        YList[int(y)] +=1 
-    final = [0 for i in range(10)]
-    for i in range(10):
-        final[i] = min(XList[i],YList[i])
-    if sum(final) == 0:
+    from collections import Counter
+    arr = []
+    A = Counter(X)
+    B = Counter(Y)
+    for k in A.keys():
+        if k in B.keys():
+            arr.append((int(k),min(A[k],B[k])))
+    if len(arr) == 0:
         return '-1'
-    if sum(final[1:]) == 0:
+    if len(arr) == 1 and arr[0][0] == 0 :
         return '0'
-    for i in range(9,-1,-1):
-        for j in range(final[i]):
-            answer+=str(i)
-    
+    arr.sort(reverse=True)
+    for d in arr:
+        for i in range(d[1]):
+            answer+=str(d[0])
     return answer
