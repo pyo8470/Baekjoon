f5ff3af15a7fe7870fcaeb84c84a6ad0808d7fec
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-03-12 23:53:44 +0900

    [Silver I] Title: 신입 사원, Time: 2292 ms, Memory: 36792 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -3,14 +3,17 @@ from collections import deque
 input = sys.stdin.readline
 for _ in range(int(input())):
     N = int(input())
-    people = [list(map(int,input().split())) for _ in range(N)]
+    people = [0 for i in range(N+1)]
+    for k in range(N):
+        a,b =map(int,input().split())
+        ## 1차 기준 정렬 되어있는 거임
+        people[a] =b 
     count = 1
-    people.sort()
     ## 2차는 무조건 높아야함
-    max = people[0][1]
-    for i in range(1,N):
-        if max > people[i][1]:
+    max = people[1]
+    for i in range(2,N+1):
+        if max > people[i]:
             count+=1
             ## [1,4] 뽑고, [4,1]
-            max = people[i][1]
+            max = people[i]
     print(count)
