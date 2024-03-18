01c2f027069c829f829c44a075ed95a990842244
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-03-18 15:43:53 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -2,7 +2,18 @@ import sys
 from itertools import combinations
 input = sys.stdin.readline
 N,M = map(int,input().split())
-L = list(i for i in range(1,N+1))
-a = combinations(L,M)
-for i in a:
-    print(*i)
+result = []
+# L = list(i for i in range(1,N+1))
+# a = combinations(L,M)
+# for i in a:
+#     print(*i)
+def dfs(start):
+    if len(result) == M:
+        print(' '.join(map(str,result)))
+        return
+    for i in range(start,N+1):
+        if i not in result:
+            result.append(i)
+            dfs(i)
+            result.pop()
+dfs(1)
