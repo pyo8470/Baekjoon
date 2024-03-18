8ae6df425949bfeebb8067f6b952ecf27baed7b4
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-03-18 15:47:36 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -3,16 +3,16 @@ from itertools import *
 input = sys.stdin.readline
 N,M = map(int,input().split())
 result = []
-L = list(i for i in range(1,N+1))
-a = combinations_with_replacement(L,M)
-for i in a:
-    print(*i)
-# def dfs(start):
-#     if len(result) == M:
-#         print(' '.join(map(str,result)))
-#         return
-#     for i in range(start,N+1):
-#             result.append(i)
-#             dfs(i)
-#             result.pop()
-# dfs(1)
+# L = list(i for i in range(1,N+1))
+# a = combinations_with_replacement(L,M)
+# for i in a:
+#     print(*i)
+def dfs(start):
+    if len(result) == M:
+        print(' '.join(map(str,result)))
+        return
+    for i in range(start,N+1):
+            result.append(i)
+            dfs(i)
+            result.pop()
+dfs(1)
