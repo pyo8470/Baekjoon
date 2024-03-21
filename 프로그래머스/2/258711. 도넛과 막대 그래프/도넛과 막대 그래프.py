685f6f68008e87aa738168dc6d3595341c92317e
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-03-22 00:13:43 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -11,25 +11,32 @@ def solution(edges):
             vertex[start] = [0,0] ## in, out
         if end not in vertex.keys():
             vertex[end] = [0,0] ## in, out
-        vertex[end][0] += 1
+        if start == end:
+            vertex[end][0] += 2
+        else:
+            vertex[end][0] += 1
         vertex[start][1] += 1
     for key in vertex.keys():
+        # print(key,vertex[key])
         edge_in,edge_out= vertex[key][0],vertex[key][1]
-        #       경우 1) out>2: ---> 이 노드가 시작 정점
+        #경우 1) out>2: ---> 이 노드가 시작 정점
         if edge_out > 2:
             answer[0] = key
-        #       경우 2) out==2:
-        #               if in>0: ---> 8자 그래프 갯수 +=1
-        #               else: ---> 이 노드가 시작 정점
+            
+        #경우 2) out==2:
+        #       if in>0: ---> 8자 그래프 갯수 +=1
+        #       else: ---> 이 노드가 시작 정점
         elif edge_out == 2:
             if edge_in > 0 :
                 answer[3] += 1
             else :
                 answer[0] = key
-        #       경우 3) out==1: 흔한 노드.. 무시
+                
+        #경우 3) out==1: 흔한 노드.. 무시
         elif edge_out == 1:
             continue
-        #       경우 4) out==0: ---> 직선 그래프 갯수 +=1
+                
+        #경우 4) out==0: ---> 직선 그래프 갯수 +=1
         elif edge_out == 0:
             answer[2]+=1
         ## 도넛의 경우
