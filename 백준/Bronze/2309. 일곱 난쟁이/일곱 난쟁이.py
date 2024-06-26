cdefff9e0f93b63ba519eb593a11b0cf9b2c9a92
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-06-26 13:31:02 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -6,8 +6,7 @@ Dwarf = []
 for i in range(9):
     Dwarf.append(int(input()))
 
-comb = combinations(Dwarf,7)
-for dwarf_comb in comb:
+for dwarf_comb in combinations(Dwarf,7):
     if sum(dwarf_comb) == 100:
         for height in sorted(dwarf_comb):
             print(height)
