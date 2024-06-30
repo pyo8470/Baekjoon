22d841b64ce70dcbdc9029554e296229a4a6d818
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-06-30 14:52:21 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,21 +1,18 @@
-a=int(input())
-string=[]
-count =0
-for i in range(a):
-  s= input()
-  string.append(s)
-for a in string:
-  last=a[0]  #마지막으로 나온 글자
-  dic={} #이미 나온 글자들
-  isbreak=False # 멈출지 말지
-  for c in a:
-    if(last!= c and dic.get(c,-1)!=-1): #딕셔너리 자료형(key value)
-      isbreak=True
-      break
-    dic[c]=dic.get(c,0)+1 # c=key 
-    last =c
-  if not isbreak:
-    count+=1
-print(count)
-# get함수 (a,b) a= 찾고싶은 딕셔너리 key 값
-# b=인자가 없을 경우 출력하고 싶은 값
+import sys
+input = sys.stdin.readline
+answer = 0
+for i in range(int(input())):
+    word = input().strip()
+    checker = set()
+    is_group_word = True
+    last = word[0]; checker.add(last)
+    for i in range(1,len(word)):
+        current = word[i]
+        if last != current and current in checker:
+            is_group_word = False
+            break
+        checker.add(current)
+        last = current
+    if is_group_word :
+        answer+=1
+print(answer)
