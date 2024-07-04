f98d36d1d224ee4d67e4bfd68c50a572cf37ceb1
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-07-04 17:37:13 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,28 +1,32 @@
-N , M = map(int,input().split())
-Board = []
-for i in range(N):
-    Board.append((input()))
+import sys
+input = sys.stdin.readline
 
-count = []
-## 두가지 경우로 나뉜다
-## 시작 색이 B,W인 경우
-for y in range(N-7):
-    for x in range(M-7):
-        startIsBlack = 0
-        startIsWhite = 0
-        for a in range(x,x+8):
-            for b in range(y,y+8):
-                ## 짝수 -> 같은 색이어야함
-                if (a+b)%2 == 0:
-                    if Board[b][a] == 'B':
-                        startIsWhite += 1
-                    else:
-                        startIsBlack +=1
-                ## 홀수 -> 다른 색이어야함
-                else:
-                    if Board[b][a] == 'B':
-                        startIsBlack += 1
-                    else:
-                        startIsWhite +=1
-        count.append(min(startIsWhite,startIsBlack))
-print(min(count))
+N, M = map(int, input().split())
+board = [input().strip() for _ in range(N)]
+
+# 두 가지 체스판 패턴
+pattern1 = [['W', 'B'] * 4, ['B', 'W'] * 4] * 4
+pattern2 = [['B', 'W'] * 4, ['W', 'B'] * 4] * 4
+
+# 각 패턴과의 차이를 저장할 배열
+diff1 = [[0] * M for _ in range(N)]
+diff2 = [[0] * M for _ in range(N)]
+
+# 차이 계산
+for i in range(N):
+    for j in range(M):
+        if board[i][j] != pattern1[i % 8][j % 8]:
+            diff1[i][j] = 1
+        if board[i][j] != pattern2[i % 8][j % 8]:
+            diff2[i][j] = 1
+
+# 최소 변경 횟수 계산
+min_changes = float('inf')
+
+for i in range(N - 7):
+    for j in range(M - 7):
+        changes1 = sum(diff1[x][y] for x in range(i, i + 8) for y in range(j, j + 8))
+        changes2 = sum(diff2[x][y] for x in range(i, i + 8) for y in range(j, j + 8))
+        min_changes = min(min_changes, changes1, changes2)
+
+print(min_changes)
