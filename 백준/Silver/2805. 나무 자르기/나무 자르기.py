031e2b62423e22bded53c6aa8b870752508dfa62
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-07-04 14:40:51 +0900

    [Silver II] Title: 나무 자르기, Time: 364 ms, Memory: 115052 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,17 +1,17 @@
 import sys
 input = sys.stdin.readline
-
+from collections import Counter
 N,M = map(int,input().split())
-woods = list(map(int,input().split()))
+woods = Counter(map(int,input().split()))
 top = max(woods)
 bottom = 0
 answer = 0
 while bottom<=top:
     cut = (top+bottom)//2
     total = 0
-    for height in woods:
+    for height, count in woods.items():
         if height > cut:
-            total+=(height - cut)
+            total+=(height - cut)*count
     if total >= M :
         ## 더 자른 경우
         answer = max(cut,answer)
