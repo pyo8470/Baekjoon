695a99fd436d8675d530c22db960f6917930ae9b
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-12-05 21:47:06 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 392 ms, Memory: 26660 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -0,0 +1,45 @@
+
+import java.io.*;
+
+public class Main {
+
+    // static BufferedWriter bw = new BufferedWriter(new
+    // OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        // StringTokenizer st = new StringTokenizer(br.readLine()); br.close();
+
+        int N = Integer.parseInt(br.readLine());
+        int answer = solution(N);
+
+        System.out.println(answer);
+    }
+
+    private static int solution(int N){
+        if (N == 1) return 2;
+        for (int i = N; i <= 1_000_000_000 ; i++) {
+            if(isPrime(i)){
+                if(isPalindrome(i)){
+                    return i;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static boolean isPrime(int num) {
+        for(int i=2; i<(int)Math.sqrt(num) + 1; i++){
+            if(num%i == 0) return false;
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(int num) {
+        String number = String.valueOf(num);
+        String number_reverse = new StringBuffer(number).reverse().toString();
+        
+        return number.equals(number_reverse);
+    }
+
+}
