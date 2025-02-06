3bba9ab8689caa878d063c2694fa374ca5951837
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-02-06 20:22:52 +0900

    [Bronze III] Title: 배수와 약수, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a == 0 && b == 0)
+                break;
+            if (b % a == 0) {
+                System.out.println("factor");
+            } else if (a % b == 0) {
+                System.out.println("multiple");
+            } else {
+                System.out.println("neither");
+            }
+        }
+    }
+}
