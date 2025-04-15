1c3bcd6ac814e573ce02212293067ed061336886
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-04-15 10:51:58 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 1397.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -1,24 +1,30 @@
-def time_to_solve(level, diff, time_cur, time_prev):
-    if level >= diff:
-        return time_cur
-    return (diff - level) * (time_cur + time_prev) + time_cur
-
+## 이분탐색
 def solution(diffs, times, limit):
-    def calculate_total_time(level):
-        total = 0
-        prev_time = 0
-        for diff, time_cur in zip(diffs, times):
-            total += time_to_solve(level, diff, time_cur, prev_time)
-            prev_time = time_cur
-        return total
-
-    min_level, max_level = 1, max(diffs)
-
-    while min_level < max_level:
-        mid_level = (min_level + max_level) // 2
-        if calculate_total_time(mid_level) <= limit:
-            max_level = mid_level
+    def 시간계산(level):
+        time_spend = 0
+        for i in range(len(diffs)):
+            diff, time_cur,time_prev = diffs[i], times[i],times[i-1]
+            ## 난이도가 낮거나 같은 경우
+            if level >= diff:
+                time_spend += time_cur
+            else:
+                wrong_count = diff-level
+                time_spend += (time_cur + time_prev)*wrong_count + time_cur
+        return time_spend
+    
+    top =max(diffs); bottom = 1; 
+    answer = 0 
+    times.append(0)
+    ## limit보다 높은 시간 -> level을 올려야함
+    ## limit보다 낮은 시간 -> 일단 answer에 넣고 level 낯추기
+    while bottom <=top :
+        level = (top+bottom)//2 
+        time_spend = 시간계산(level)
+        if time_spend > limit:
+            ## level 갱신
+            bottom = level + 1
         else:
-            min_level = mid_level + 1
-
-    return min_level
+            answer = level
+            top = level - 1
+        
+    return answer
