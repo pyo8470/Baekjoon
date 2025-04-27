e3ef8aeb6b4c0c04bbefea13998905f1c9aad3e1
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-04-27 17:46:21 +0900

    [level 2] Title: 완전범죄, Time: 7.08 ms, Memory: 9.85 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -3,15 +3,21 @@ def solution(info, n, m):
     INF = float('inf')
     answer = INF
     
-    @lru_cache(maxsize=None)
+    visited = dict()
+    
     def dfs(A,B,depth):
         nonlocal answer
         if A>=n or B>=m:
             return
+        if A>=answer:
+            return
         if depth == len(info):
             answer = min(A,answer)
             return
+        if (A,B,depth) in visited:
+            return
         
+        visited[(A,B,depth)] = True
         a_trace, b_trace = info[depth]
         
         dfs(A+a_trace,B,depth+1)
