f39afebf71d3b514d6e47d562c737f622b2bb9dd
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-07-22 14:33:50 +0900

    [Gold V] Title: AC, Time: 36 ms, Memory: 3536 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,84 +1,83 @@
-#include <bits/stdc++.h>
-#include <climits>
-
+#include <iostream>
 using namespace std;
+#include <string>
+#include <vector>
+#include <stack>
+#include <unordered_set>
 
-int main()
-{
-    int T;
-    cin >> T;
+void parsing(deque<int>& dq, string input);
+string solution();
+string solution() {
+	string command; cin >> command;
+	int n; cin >> n;
+	string input; cin >> input;
+	deque<int> dq;
+	
+	parsing(dq,input);
+	
+	bool isReversed = false;
+	for (int i = 0; i < command.size(); i++) {
+		if (command[i] == 'R') {
+			isReversed = !isReversed;
+		}
+		else{
+			if (isReversed) {
+				if (dq.empty()) return "error";
+				dq.pop_back();
+			}
+			else {
+				if (dq.empty()) return "error";
+				dq.pop_front();
+			}
+		}
+	}
+	string result = "[";
+	int length = dq.size();
+	if (isReversed) {
+		for (int i = length-1; i >= 0 ; i--) {
+			if (i != 0) {
+				result += (to_string(dq[i])+",");
+			}
+			else {
+				result += to_string(dq[i]);
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < length; i++) {
+			if (i != length-1) {
+				result += (to_string(dq[i]) + ",");
+			}
+			else {
+				result += to_string(dq[i]);
+			}
+		}
+	}
+	result += "]";
+	return result;
+}
 
-    for (int tc = 0; tc < T; tc++)
-    {
-        int n;
-        string p;
+void parsing(deque<int> &dq,string input) {
+	string k = "";
+	for (int i = 0; i < input.size(); i++) {
+		if (isdigit(input[i])) {
+			k += input[i];
+		}
+		else {
+			if (!k.empty()) {
+				dq.push_back(stoi(k));
+				k = "";
+			}
+		}
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-        cin >> p;
-        cin >> n;
-        deque<int> arr;
-        string input;
-        cin >> input;
-        string num = "";
-        for (char ch : input)
-        {
-            if (isdigit(ch))
-            {
-                num += ch;
-            }
-            else if (!num.empty())
-            {
-                arr.push_back(stoi(num));
-                num = "";
-            }
-        }
-
-        bool flag = true;
-        bool reversed = false;
-        for (char command : p)
-        {
-            // 연속적인 R 탐색
-            if (command == 'R')
-            {
-                reversed = !reversed;
-            }
-            else
-            {
-                if (arr.size() > 0)
-                {
-                    if (!reversed)
-                    {
-                        arr.pop_front();
-                    }
-                    else
-                    {
-                        arr.pop_back();
-                    }
-                }
-                else
-                {
-                    flag = false;
-                    break;
-                }
-            }
-        }
-        if (flag)
-        {
-            cout << '[';
-            if (reversed)
-                reverse(arr.begin(), arr.end());
-            for (int i = 0; i < arr.size(); i++)
-            {
-                cout << arr[i];
-                if (i != (arr.size() - 1))
-                {
-                    cout << ',';
-                }
-            }
-            cout << ']' << endl;
-        }
-        else
-        {
-            cout << "error" << endl;
-        }
-    }
+	int T; cin >> T;
+	for (int t = 0; t < T; t++) {
+		cout << solution() << endl;
+	}
 }
