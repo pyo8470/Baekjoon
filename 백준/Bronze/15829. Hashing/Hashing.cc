bfee10531a0aa56a60ba04ba0dd553b65b347b5d
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-07-23 10:23:08 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -9,15 +9,20 @@
 #include <math.h>
 using namespace std;
 
-int main(){
+
+int main() {
 	int L; cin >> L;
 	string input; cin >> input;
-	
-	int result = 0;
+
+	long long result = 0;
 	int MOD = 1234567891;
+	long long pow = 1;
 	for (int i = 0; i < L; i++) {
-		int k = (input[i] - 'a' + 1) * (int)pow(31, i) % MOD;
-		result += k;
+		long long k = (long long)(input[i] - 'a' + 1);
+		result = (result + k * pow) % MOD;
+		pow = (pow * 31) % MOD;
+		
 	}
 	cout << result;
 }
+
