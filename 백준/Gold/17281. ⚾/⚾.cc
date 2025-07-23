bece9376f68659eaa3144f603e7aad8fc819eccc
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-07-23 09:29:03 +0900

    [Gold IV] Title: ⚾, Time: 268 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.cc
+++ 백준/Gold/17281. ⚾/⚾.cc
@@ -8,89 +8,91 @@
 #include <algorithm>
 
 using namespace std;
-int N;
-int maxOut;
 // 타순에 해당하는 번호
 // 입력으로 들어오는것 -> 등번호에 해당하는 성적
-vector<vector<int>> play;
+int N;
+// 1번선수는 4번타자 고정이기 때문에 뺀다.
 
-int calculation(vector<int>& order) {
-	// 점수 계산
-
+vector<vector<int>> playing;
+// 타순에 따른 배팅
+int playBall(vector<int> &lineup) { // 라인업
 	int score = 0;
 	int nextPlayer = 0;
 	for (int inning = 0; inning < N; inning++) {
 		int outCount = 0;
-		// 1,2,3
+		// 베이스 1 2 3루
+		// 
 		bool base[3] = { false, };
-
-
+		// 3아웃 되기 전까지 무한
 		while (outCount < 3) {
-			int player = order[nextPlayer];
-			int result = play[inning][player];
-
+			int player = lineup[nextPlayer]; // 선수의 번호
+			int result = playing[inning][player];
 			if (result == 0) {
 				outCount++;
 			}
-			else {
-				if (result == 1) {
-					if (base[2]) score++;
-					base[2] = base[1];
-					base[1] = base[0];
-					base[0] = true;
-				}
-				else if (result == 2) {
-					if (base[2]) score++;
-					if (base[1]) score++;
-					base[2] = base[0];
-					base[1] = true;
-					base[0] = false;
-				}
-				else if (result == 3) {
-					
-					for (int i = 0; i < 3; i++) {
-						if (base[i]) score++;
-						base[i] = false;
-					}
-					base[2] = true;
-				}
-				else if (result == 4) {
-					for (int i = 0; i < 3; i++) {
-						if (base[i]) score++;
-						base[i] = false;
-					}
-					score++;
 
+			else if (result == 1) {
+				if (base[2]) score++;
+				base[2] = base[1];
+				base[1] = base[0];
+				base[0] = true;
+			}
+			else if (result == 2) {
+				if (base[2]) score++;
+				if (base[1]) score++;
+				base[2] = base[0];
+				base[1] = true;
+				base[0] = false;
+			}
+			else if (result == 3) {
+				for (int i = 0; i < 3; i++) {
+					if (base[i]) score++;
+					base[i] = false;
 				}
+				base[2] = true;
+			}
+			else if (result == 4) {
+				for (int i = 0; i < 3; i++) {
+					if (base[i]) score++;
+					base[i] = false;
+				}
+				score++;
 			}
 			nextPlayer = (nextPlayer + 1) % 9;
 		}
 	}
 	return score;
 }
+
 int main() {
 	// 야구
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	cin >> N;
-	maxOut = N * 2;
-	play.assign(N, vector<int>(9));
+
+	vector<int> order = { 1,2,3,4,5,6,7,8 };
+	playing.assign(N,vector<int>(9));
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < 9; j++) {
-			cin >> play[i][j];
+		for(int j=0; j<9; j++){
+			cin >> playing[i][j];
 		}
 	}
 
-	// 타순 돌리기
 	int maxScore = 0;
-	vector<int> order = { 1,2,3,4,5,6,7,8 };
+	// 모든 조합에 대해서 루프 돌리기
 	do {
-		vector<int> batting(9);
-		for (int i = 0, j = 0; i < 9; i++) {
-			// 4번타자 -> 등번호 1번
-			if (i == 3) batting[i] = 0;
-			else batting[i] = order[j++];
+		vector<int> lineup(9);
+		int ord = 0;
+		for (int i = 0;i < 9; i++) {
+			if (i == 3) lineup[i] = 0;
+			else lineup[i] = order[ord++];
 		}
-		maxScore = max(maxScore, calculation(batting));
+
+		maxScore = max(playBall(lineup), maxScore);
 	} while (next_permutation(order.begin(), order.end()));
+
 	cout << maxScore;
 }
 
