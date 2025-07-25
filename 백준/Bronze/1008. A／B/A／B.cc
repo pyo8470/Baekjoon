b362a91508df572e7faee5e690e069c7d6fbd7d6
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-07-25 16:23:29 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.precision(10);
+	long double a, b;
+	cin >> a >> b;
+	cout << (a/b);
+}
