714095a321ad4007807f4249926637413b0c05e8
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-07-25 16:42:51 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	//int n; cin >> n;
+
+	cout << "\\    /\\\n";
+	cout << " )  ( ')\n";
+	cout << "(  /  )\n";
+	cout << " \\(__)|\n";
+
+}
