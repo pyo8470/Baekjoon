02fd3b1985586f2fdfddc17a3dbb6c3332373eba
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-07-25 16:46:26 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	//int n; cin >> n;
+
+	/*cout << "\\    /\\\n";
+	cout << " )  ( ')\n";
+	cout << "(  /  )\n";
+	cout << " \\(__)|\n";*/
+
+	cout << "|\\_/|\n";
+	cout << "|q p|   /}\n";
+	cout << "( 0 )\"\"\"\\\n";
+	cout << "|\"^\"`    |\n";
+	cout << "||_/=\\\\__|\n";
+}
