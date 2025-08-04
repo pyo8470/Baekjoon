4a76f99b952f479f1edca1b32608bfc613a634f8
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-08-04 10:14:24 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+
+//#include <bits/stdc++.h>
+using namespace std;
+int N, M;
+vector<bool> visited;
+
+void NM(int cur, vector<int> answer ,int depth) {
+	if (depth == M) {
+		for (int k : answer) {
+			cout << k << " ";
+		}
+		cout << '\n';
+	}
+
+	for (int i = cur; i <= N; i++) {
+		answer.push_back(i);
+		NM(i + 1, answer, depth + 1);
+		answer.pop_back();
+	}
+}
+int main() {
+	cin >> N >> M;
+
+	visited.assign(N, false);
+	NM(1, {}, 0);
+}
