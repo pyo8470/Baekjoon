33a2739399270ca89a67d6c0192eb3c65c2f659f
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-08-05 09:33:16 +0900

    [Silver II] Title: 주식, Time: 576 ms, Memory: 10360 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.cc
+++ 백준/Silver/11501. 주식/주식.cc
@@ -4,7 +4,11 @@ using namespace std;
 int N;
 vector<int> price;
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 	int T; cin >> T;
+
 	for (int t = 0; t < T; t++) {
 		cin >> N; price.resize(N, 0);
 		for (int i = 0; i < N; i++) cin >> price[i];
@@ -13,13 +17,8 @@ int main() {
 		int MAX = price[N-1];
 		for (int i = N - 2; i >= 0; i--) {
 			int current = price[i];
-			//cout << current << " " << MAX << '\n';
-			if (current < MAX) {
-				answer += MAX - current;
-			}
-			else {
-				MAX = current;
-			}
+
+			(current < MAX) ? answer += MAX - current : MAX = current;
 		}
 		cout << answer << '\n';
 	}
