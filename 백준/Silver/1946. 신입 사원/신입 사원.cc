d48e07ff65c4f9625b9733f6dc4ebc3fc59b262d
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-08-13 09:34:25 +0900

    [Silver I] Title: 신입 사원, Time: 456 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -12,7 +12,9 @@ struct applicant {
 };
 vector<applicant> app;
 int main() {
-
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 	int T; cin >> T;
 	for (int t = 0; t < T; t++) {
 		int N; cin >> N;
