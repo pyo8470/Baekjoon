630bf1252480eac963a933efabb33b3dc68adcec
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-08-13 10:30:11 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 404 ms, Memory: 20172 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include<algorithm>;
+using namespace std;
+
+typedef pair<int, int> pii;
+int N, M, K, P;
+vector<vector<int>> adj;
+vector<bool> visited;
+priority_queue<int,vector<int>,greater<int>> result;
+void bfs(const int& start) {
+	queue<pii> q;
+	q.push({ start,0 });
+
+	while (!q.empty()) {
+		pii cur = q.front(); q.pop();
+		int node = cur.first;
+		int cost = cur.second;
+		if (cost == K) {
+			result.push(node);
+		}
+		visited[node] = true;
+		for (int next : adj[node]) {
+			if (visited[next]) continue;
+			if (cost + 1 > K) continue;
+
+			q.push({ next,cost + 1 });
+			visited[next] = true;
+		}
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N >> M >> K >> P;
+	adj.assign(N + 1, vector<int>());
+	visited.assign(N + 1, false);
+	for (int i = 0; i < M; i++) {
+		int s, e; cin >> s >> e;
+		adj[s].push_back(e);
+	}
+	
+	visited[P] = true;
+	bfs(P);
+
+	if (result.empty()) {
+		cout << -1 << '\n';
+	}
+	while (!result.empty()) {
+		cout << result.top() << '\n';
+		result.pop();
+	}
+}
