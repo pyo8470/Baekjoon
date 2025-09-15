3beef84677dcbed3cf0363c4d7f2420051498f27
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-09-15 13:35:43 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 176 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+#include<algorithm>
+#include<deque>
+#include<vector>
+#define MAX_LEN 100000
+using namespace std;
+
+
+int N, K;
+int ans = 0;
+
+struct State{
+	bool robot = false;
+	int dur;
+};
+deque<State> belt;
+
+void print() {
+	cout << "print()" << '\n';
+	for (State& cur : belt) {
+		cout << "내구도 : " << cur.dur << " robot : " << cur.robot << '\n';
+	}cout << '\n';
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> N >> K;
+	// 시작점을 잡아야하는데
+	
+	for (int i = 0; i < 2 * N; i++) {
+		int dur; cin >> dur;
+		belt.push_back({false,dur});
+	}
+
+	while (true) {
+		// 벨트 회전
+		ans++;
+		/*cout << "단계 " << ans << '\n';
+		print();*/
+
+		belt.push_front(belt.back());
+		belt.pop_back();
+		// 내리는 위치
+		if (belt[N - 1].robot) belt[N - 1].robot = false;
+		/*cout << "회전 후" << "\n";
+		print();*/
+
+
+		// 로봇 움직임
+		
+		for (int i = 2 * N - 1; i >= 0; i--) {
+			if (!belt[i].robot) continue;
+			int next_idx = (i + 1) % (2 * N);
+
+			if (belt[next_idx].dur < 1 || belt[next_idx].robot) continue;
+
+			belt[next_idx].dur = max(0,belt[next_idx].dur-1);
+			belt[next_idx].robot = true;
+			// 내리는 위치
+			if(next_idx == N-1) belt[next_idx].robot = false;
+			belt[i].robot = false;
+		}
+		/*cout << "로봇 움직임 후" << "\n";
+		print();*/
+
+		if (belt[0].dur != 0) {
+			belt[0].robot = true;
+			belt[0].dur = max(0, belt[0].dur - 1);
+		}
+		//print();
+
+		int broken = 0;
+		for (int i = 0; i < 2 * N; i++) {
+			if (belt[i].dur <= 0) broken++;
+			if (broken >= K) {
+				cout << ans << '\n';
+				return 0;
+			}
+		}
+	}
+}
+
