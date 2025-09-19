736c072944db99f2c193ca2490787ed7f73c12ec
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2025-09-19 11:52:45 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N; cin >> N;
+
+	priority_queue<int> pq;
+	for (int i = 0; i < N; i++) {
+		int x; cin >> x;
+		if (x == 0) {
+			if (!pq.empty()) {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+			else cout << 0 << '\n';
+		}
+		else {
+			pq.push(x);
+		}
+	}
+}
