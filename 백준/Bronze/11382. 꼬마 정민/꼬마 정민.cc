49495671666e56fb394711ae202b9589e3889f4e
yoonpyo hong <84065357+pyo8470@users.noreply.github.com>
2025-10-13 11:19:13 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+}
