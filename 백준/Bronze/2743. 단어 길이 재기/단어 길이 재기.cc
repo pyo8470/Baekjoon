84c9ac4c223ed5f85c255e66892f5d0c8b63c631
yoonpyo hong <84065357+pyo8470@users.noreply.github.com>
2025-10-14 17:01:24 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+void solve(){
+    string input; cin >> input;
+    cout << input.size();
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
