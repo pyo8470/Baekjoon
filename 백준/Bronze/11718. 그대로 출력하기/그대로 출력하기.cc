a18821cbd768a5845e2784d561908fa8b15d7469
yoonpyo hong <84065357+pyo8470@users.noreply.github.com>
2025-10-14 17:36:02 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(){
+    string input;
+    while(getline(cin,input)){
+        cout << input << '\n';
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
