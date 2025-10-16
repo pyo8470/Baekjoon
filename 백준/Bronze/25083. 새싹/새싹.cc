38d6f54eadfa1d848c9e1fbfeda2b11aa6e40a72
yoonpyo hong <84065357+pyo8470@users.noreply.github.com>
2025-10-16 09:00:22 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+// 문자열을 구분자로 나누는 함수
+
+// 주 로직
+void solve() {
+    cout << "         ,r'\"7\n";
+    cout << "r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+}
