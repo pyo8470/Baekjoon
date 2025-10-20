07d36f642760f7056805fc542eab4e923b917474
yoonpyo hong <84065357+pyo8470@users.noreply.github.com>
2025-10-20 14:54:23 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,29 +1,36 @@
-#include <bits/stdc++.h>
-#include <climits>
-#include <math.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int N,M;
-int result = 0;
-int main() {
-   
-   cin >> N >> M;
-
-   int nums[N];
-
-   for(int i=0; i<N; i++){
-    cin >> nums[i];
-   }
-
-   for(int i=0; i<N-2; i++){
-    for(int j=i+1; j<N-1;j++){
-        for(int k = j+1; k<N;k++){
-            int sum = nums[i] + nums[j] + nums[k];
-            result = (sum <= M && abs(M-sum) < abs(M-result)) ? sum : result;
-        }
-    }
-   }
-   printf("%d", result);
+int N, M;
+vector<int> cards;
+int ans = 0;
+void init() {
+    cin >> N >> M;
+    cards.resize(N);
+    for (int i = 0; i < N; i++) cin >> cards[i];
 }
 
+void bruteForce(const int &depth, const int &idx, const int &sum) {
+    if (sum > M)
+        return;
+    if (depth == 3) {
+        ans = max(ans, sum);
+        return;
+    }
 
+    for (int i = idx; i < N; i++) {
+        bruteForce(depth + 1, i + 1, sum + cards[i]);
+    }
+}
+void solve() {
+    bruteForce(0, 0, 0);
+    cout << ans;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    init();
+    solve();
+}
