cbb81598f9b9bd3a9ee02a7c0a376fd1e9757dc8
yoonpyo hong <84065357+pyo8470@users.noreply.github.com>
2025-10-20 14:11:01 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  cout << 1 << '\n' << 0;
+}
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+}
