2c44b715f9e921bdd4985192c54fe531148de775
yoonpyo hong <84065357+pyo8470@users.noreply.github.com>
2025-10-20 14:58:27 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -1,22 +1,31 @@
-#include <bits/stdc++.h>
-#include <climits>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main() {
-    int N;
-    cin >> N;
 
-    for(int i=1; i<=N ; i++){
-        int num = i;
-        int sum = num;
-        while(num > 0){
-            sum += num%10;
-            num /= 10;
-        }
-        if (sum == N){
-            cout << i; 
-            return 0;
+int N;
+void init() { cin >> N; }
+int getSum(int n) {
+    int ret = n;
+    while (n) {
+        ret += n % 10;
+        n /= 10;
+    }
+    return ret;
+}
+void solve() {
+    for (int i = 1; i <= N; i++) {
+        int k = getSum(i);
+        if (k == N) {
+            cout << i;
+            return;
         }
     }
-
     cout << 0;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    init();
+    solve();
 }
