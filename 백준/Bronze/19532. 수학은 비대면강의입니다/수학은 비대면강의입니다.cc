945aa8a2e84157ee746976254c46fbd92849c6d4
yoonpyo hong <84065357+pyo8470@users.noreply.github.com>
2025-10-20 15:09:36 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -1,21 +1,25 @@
-#include <bits/stdc++.h>
-#include <climits>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main()
-{
-    double a, b, c, d, e, f;
+
+void init() {}
+void solve() {
+    int a, b, c, d, e, f;
     cin >> a >> b >> c >> d >> e >> f;
 
-    // 브루트포스 말고
+    for (int x = -999; x <= 999; x++) {
+        for (int y = -999; y <= 999; y++) {
+            if ((a * x + b * y == c) && (d * x + e * y == f)) {
+                cout << x << " " << y;
+                return;
+            }
+        }
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    // ax + by = c
-    // dx + ey = f
-
-    // a,d 계수 맞추기
-
-    double denominator = a*e - b*d;
-
-    int x = (c * e - b * f) / denominator;
-    int y = (a * f - c * d) / denominator;
-    printf("%d %d", x, y);
+    init();
+    solve();
 }
