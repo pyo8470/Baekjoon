33ace27b82bf16527c4ee81631453d1fccf774e0
yoonpyo hong <84065357+pyo8470@users.noreply.github.com>
2025-10-21 09:06:01 +0900

    [Silver V] Title: 숫자 카드, Time: 368 ms, Memory: 21800 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+    unordered_set<int> cards;
+    cards.reserve(500005);
+    for (int i = 0; i < N; i++) {
+        int c;
+        cin >> c;
+        cards.insert(c);
+    }
+
+    int M;
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        int target;
+        cin >> target;
+        if (cards.find(target) == cards.end())
+            cout << 0 << " ";
+        else
+            cout << 1 << " ";
+    }
+}
