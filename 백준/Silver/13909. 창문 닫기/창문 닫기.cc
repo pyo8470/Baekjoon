ac3ce7602467299a567efbdee4848d34e8b72ec2
yoonpyo hong <84065357+pyo8470@users.noreply.github.com>
2025-10-27 15:25:00 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+    cout << (int)sqrt(N);
+}
